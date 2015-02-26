4302374dd18439eda7a35065219b8f18863eea7a
kongfushi <kongfushi@gmail.com>
2015-02-26 19:37:16 +0800

    kernel compile script for i9308
    Change-Id: Iebdaa23690e1b57b8fa6e56be0ef5230b545db07

--- buildkernel.sh
+++ buildkernel.sh
@@ -1,4 +1,5 @@
 #for some reason,currently kernel must build seprately
+
 ARGS="-C kernel/samsung/smdk4412 ARCH=arm CROSS_COMPILE=$(pwd)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-"
 mka $ARGS VARIANT_DEFCONFIG= SELINUX_DEFCONFIG= cyanogenmod_i9308_defconfig
 mka $ARGS zImage
@@ -7,11 +8,10 @@ mka $ARGS modules
 mka $ARGS INSTALL_MOD_PATH=$(pwd)/out/target/product/i9308/system modules_install
 
 cp kernel/samsung/smdk4412/arch/arm/boot/zImage vendor/samsung/i9308/proprietary/zImage
-dest_dir=$(pwd)/vendor/samsung/i9308/proprietary/system/lib/modules/
+dest_dir=$(pwd)/vendor/samsung/i9308/proprietary/system/lib/modules
 for f in $(find kernel/samsung/smdk4412 -name *.ko); do
     echo copy $f to $dest_dir
     cp $f $dest_dir
 done
-
 
 
